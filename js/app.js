086f4bb6ed6ec386dd822924862596ae7c27bde8
Hugo Barrera <exailer89@gmail.com>
2024-04-01 23:48:20 -0600

    Creando el objeto de Cita para sincronizar formulario y datos

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@ const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
+
+// Objeto de Cita
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: ''
+}
+
+console.log(citaObj);
